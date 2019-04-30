76d4148eef01078505c054623afa1f636a123afa
RayB <raybernard1337@gmail.com>
2019-04-30 14:58:33 -0600

    Added GetStarted section

---
+++ src/components/pages/index/GetStarted.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+const GetStarted = () => (
+  <section id="getstarted" className="p-5 text-center text-white">
+    <div className="dark-overlay">
+      <div className="row">
+        <div className="col">
+          <div className="container pt-5">
+            <h2 className="h1">Are You Ready To Get Started?</h2>
+            <p className="d-none d-md-block">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+              veritatis quo culpa ut expedita natus laborum fugit sequi nobis
+              sint!
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+)
+
+export default GetStarted
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Header from "../components/pages/index/Header"
 import Features from "../components/pages/index/Features"
+import GetStarted from "../components/pages/index/GetStarted"
 
 const IndexPage = () => (
   <Layout>
@@ -12,6 +13,7 @@ const IndexPage = () => (
     <Header />
     <main>
       <Features />
+      <GetStarted />
     </main>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
