0182209760cac1b15c2a2a21d823c1b869a172d9
RayB <raybernard1337@gmail.com>
2019-05-04 08:51:05 -0600

    Added contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import PageHeader from "../components/common/PageHeader"
+
+const services = () => (
+  <Layout>
+    <SEO title="Contact" keywords={[`contact`, `glozzmo`]} />
+    <PageHeader pageTitle="Contact" />
+  </Layout>
+)
+
+export default services
